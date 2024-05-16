1942b613276739bfc3732e6b2a72291c8a5166c0
Felix Dilly <dilly.felix@fronius.com>
2024-05-16 13:18:54 +0200

    fix: forgot event class uuid

--- include/utils/events.h
+++ include/utils/events.h
@@ -19,7 +19,8 @@ static PresetEventLogger logger;
 
 namespace events {
 class InstanceStarted : public Event {
-    static inline EventConfig _cfg{"123Everest123", "A new Everest Instance successfully started.", Severity::Debug,
+    static inline EventConfig _cfg{"54efd4cc-03ff-4491-9a39-c29f8fd9d02d",
+                                   "A new Everest Instance successfully started.", Severity::Debug,
                                    EventGroup::Default};
 
 public:
@@ -29,8 +30,8 @@ public:
 };
 
 class ConnectionMqttFailed : public Event {
-    static inline EventConfig _cfg{"", "Instance Failed to connect to MQTT-Broker", Severity::Error,
-                                   EventGroup::Default};
+    static inline EventConfig _cfg{"f2c6ca2c-214e-48c0-ba6a-1f9534420ad7", "Instance Failed to connect to MQTT-Broker",
+                                   Severity::Error, EventGroup::Default};
 
 public:
     explicit ConnectionMqttFailed(const std::string nodeid, std::string host, int port) : Event(_cfg) {
@@ -41,7 +42,8 @@ public:
 };
 
 class LoadingConfigFailed : public Event {
-    static inline EventConfig _cfg{"", "Instance failed to load its config!", Severity::Error, EventGroup::Default};
+    static inline EventConfig _cfg{"f3d11fa4-013a-42d9-b7d8-1e60b8bfde59", "Instance failed to load its config!",
+                                   Severity::Error, EventGroup::Default};
 
 public:
     explicit LoadingConfigFailed(std::string nodeid) : Event(_cfg) {
@@ -50,7 +52,8 @@ public:
 };
 
 class InstanceModuleFailed : public Event {
-    static inline EventConfig _cfg{"", "A Module of the Instance failed!", Severity::Trace, EventGroup::Default};
+    static inline EventConfig _cfg{"494cd1bf-c161-4bbe-84f9-b6857e04a30a", "A Module of the Instance failed!",
+                                   Severity::Trace, EventGroup::Default};
 
 public:
     explicit InstanceModuleFailed(std::string nodeid, std::string moduleName, int wstatus) : Event(_cfg) {
@@ -61,7 +64,8 @@ public:
 };
 
 class EverestInstanceCrashed : public Event {
-    static inline EventConfig _cfg{"", "Instance crashed", Severity::Fatal, EventGroup::Default};
+    static inline EventConfig _cfg{"bd3adf23-9af4-45df-b967-e049d167416d", "Instance crashed", Severity::Fatal,
+                                   EventGroup::Default};
 
 public:
     explicit EverestInstanceCrashed(std::string nodeid) : Event(_cfg) {
@@ -70,7 +74,8 @@ public:
 };
 
 class OcppConnected : public Event {
-    static inline EventConfig _cfg{"", "Instance established connection with OCPP backend.", Severity::Info,
+    static inline EventConfig _cfg{"b7263f4b-21be-4828-9cb7-fb93d1f075f0",
+                                   "Instance established connection with OCPP backend.", Severity::Info,
                                    EventGroup::Default};
 
 public:
@@ -80,7 +85,8 @@ public:
 };
 
 class OcppNotConnected : public Event {
-    static inline EventConfig _cfg{"", "Instance has no connection to the OCPP backend.", Severity::Error,
+    static inline EventConfig _cfg{"9b5a3f65-f92e-4822-a186-1b0f2dd12d7f",
+                                   "Instance has no connection to the OCPP backend.", Severity::Error,
                                    EventGroup::Default};
 
 public:
